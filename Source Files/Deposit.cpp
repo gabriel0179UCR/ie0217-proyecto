26191192173b47608dbf37d627514778cae399d7
Gabriel Angulo <gabrilo0179@gmail.com>
2024-06-11 18:51:40 -0600

    Funcionalidad de depositar en cuenta .cpp
    Signed-off-by: Gabriel Angulo <gabrilo0179@gmail.com>

---
+++ Source Files/Deposit.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <sqlite3.h>
+#include <fstream>
+#include <string>
+#include <regex>
+using namespace std;
+
+string read_sql_file(const string& filename) {
+  string content;
+  try {
+    ifstream file(filename);
+    if (file.is_open()) {
+      string line;
+      while (getline(file, line)) {
+        content += line + "\n";
+      }
+      file.close();
+    } else {
+      cerr << "Error: Could not open file '" << filename << "'" << endl;
+    }
+  } catch (const exception& e) {
+    cerr << "Error: General: " << e.what() << endl;
+  }
+  return content;
+}
+
+static int callback(void *data, int argc, char **argv, char **azColName) {
+  for (int i = 0; i < argc; i++) {
+      cout << azColName[i] << " = " << (argv[i] ? argv[i] : "NULL") << endl;
+  }
+  return 0;
+}
+
+void deposit(sqlite3 *db, string clienteID, string denominacionID, string cantidad) {
+    const char* data = "Callback function called";
+    char *errMsg = 0;
+    int rc;
+    string query = read_sql_file(".\\SQL_Scripts\\Deposit.sql");
+    query = regex_replace(query, regex("\\{0\\}"), clienteID); 
+    query = regex_replace(query, regex("\\{1\\}"), denominacionID); 
+    query = regex_replace(query, regex("\\{2\\}"), cantidad); 
+    cout << query << endl;
+    const char *sql = query.c_str();
+    rc = sqlite3_exec(db, sql, callback, (void*)data, &errMsg);
+    if (rc != SQLITE_OK) {
+        cerr << "SQL error: " << errMsg << endl;
+        sqlite3_free(errMsg);
+    } else {
+        cout << "Operation done successfully" << endl;
+    }
+}
+
+//int main(int argc, char* argv[]) {
+//
+//    sqlite3 *db;
+//    int rc;
+//    rc = sqlite3_open("banco_ie0217_db.db", &db);
+//    deposit(db, "1", "Colones", "500");
+//
+//    sqlite3_close(db);
+//    return 0;
+//}
