06d2be741e660f394f95f62f5e533a64ac0a97ee
Gabriel Angulo <gabrilo0179@gmail.com>
2024-06-11 18:52:08 -0600

    Query que permite depositar en cuenta .sql
    Signed-off-by: Gabriel Angulo <gabrilo0179@gmail.com>

---
+++ SQL_Scripts/Deposit.sql
@@ -0,0 +1,24 @@
+/*
+Deposit
+
+Query to update the quantity in the account
+
+Parameters
+- {0}: Client ID
+- {1}: Denomination (ex. 'Colones')
+- {2}: Quantity
+*/
+
+-- Update row in Cuentas
+UPDATE Cuentas SET Cantidad = {2} WHERE ClientesID = {0} AND DenominacionID = (
+    SELECT ID AS DenominacionID FROM Denominaciones WHERE Denominacion = '{1}'
+);
+
+-- Show the new quantity in the count
+SELECT ClientesID, D.Denominacion, Cantidad
+FROM Cuentas C
+LEFT JOIN Denominaciones D
+ON C.DenominacionID = D.ID
+WHERE ClientesID = {0}
+    AND D.Denominacion = '{1}'
+;
